961eb64b2f78fbe87aee6526206bc5c0d7cd2ad3
lau2413 <samoaco2413@gmail.com>
2025-10-28 12:59:41 -0500

    se añade init y arreglo dle modelo

--- src/models/model_b.py
+++ src/models/model_b.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from utils import load_chunks_from_folder
+from src.utils import load_chunks_from_folder
 
 class RAGModelB:
     """
